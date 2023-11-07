38317478308cb397f13720e046e10825b8b041a7
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2023-11-07 15:09:22 +0000

    added portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,69 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter/services.dart';
 
 import 'package:xpense_app/widgets/expenses.dart';
 
-var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+    seedColor: Color.fromARGB(255, 5, 99, 125), brightness: Brightness.dark);
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData().copyWith(
-        useMaterial3: true,
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: kColorScheme.onSecondaryContainer,
-                fontSize: 16,
+  /*WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (value) {*/
+      runApp(
+        MaterialApp(
+          debugShowCheckedModeBanner: false,
+          darkTheme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: Color.fromARGB(255, 40, 40, 40),
+            useMaterial3: true,
+            cardTheme: const CardTheme().copyWith(
+              color: Colors.black38,
+              margin: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 8,
               ),
             ),
-      ),
-      home: const Expenses(),
-    ),
-  );
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: kDarkColorScheme.primaryContainer,
+                  foregroundColor: kDarkColorScheme.onPrimaryContainer),
+            ),
+            colorScheme: kDarkColorScheme.copyWith(error: Colors.red),
+          ),
+          theme: ThemeData().copyWith(
+            useMaterial3: true,
+            colorScheme: kColorScheme,
+            appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.primaryContainer,
+            ),
+            cardTheme: const CardTheme().copyWith(
+              color: kColorScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 8,
+              ),
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kColorScheme.primaryContainer,
+              ),
+            ),
+            textTheme: ThemeData().textTheme.copyWith(
+                  titleLarge: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: kColorScheme.onSecondaryContainer,
+                    fontSize: 16,
+                  ),
+                ),
+          ),
+          themeMode: ThemeMode.system, // default setting
+          home: const Expenses(),
+        ),
+      );
+    //},
+ //);
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:xpense_app/widgets/expenses_list/expenses_list.dart';
 import 'package:xpense_app/widgets/new_expense.dart';
 import 'package:xpense_app/models/expense_model.dart';
-
+import 'chart/chart.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -26,72 +26,82 @@ class _ExpensesState extends State<Expenses> {
         amount: 11.99,
         date: DateTime.now(),
         category: Category.leisure),
+    Expense(
+        title: 'Business trip',
+        amount: 149.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(title: 'Flight to Dubai', amount: 459.45, date: DateTime.now(), category: Category.travel),
+    Expense(title: 'American Style', amount: 29, date: DateTime.now(), category: Category.food)
   ];
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
-      isScrollControlled: true,
-      context: context, 
-      builder: (context) => NewExpense(onAddExpense: _addExpense)
-    );
+        isScrollControlled: true,
+        context: context,
+        builder: (context) => NewExpense(onAddExpense: _addExpense));
   }
 
-  void _addExpense(Expense newExpense){
+  void _addExpense(Expense newExpense) {
     setState(() {
       _registeredExpenses.add(newExpense);
     });
   }
 
-  void _removeExpense(Expense expense){
+  void _removeExpense(Expense expense) {
     final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
     ScaffoldMessenger.of(context).clearSnackBars();
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        duration: const Duration(seconds: 3),
-        content: const Text('Expense deleted'),
-        action: SnackBarAction(
-          label: 'Undo',
-          onPressed: () {
-            setState(() {
-              _registeredExpenses.insert(expenseIndex, expense);
-            });
-          },
-        ),
-      ));
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      duration: const Duration(seconds: 3),
+      content: const Text('Expense deleted'),
+      action: SnackBarAction(
+        label: 'Undo',
+        onPressed: () {
+          setState(() {
+            _registeredExpenses.insert(expenseIndex, expense);
+          });
+        },
+      ),
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget mainContent = const Center(
-      child: Text('No expenses found. Start adding some')
-    );
+    final width = MediaQuery.of(context).size.width;
 
-    if(_registeredExpenses.isNotEmpty) {
+    Widget mainContent =
+        const Center(child: Text('No expenses found. Start adding some'));
+
+    if (_registeredExpenses.isNotEmpty) {
       mainContent = ExpensesList(
-      expenses: _registeredExpenses,
-      onRemoveExpense: _removeExpense,
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
       );
     }
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Expense Tracker'),
-        actions: [
-          IconButton(
-            onPressed: _openAddExpenseOverlay,
-            icon: const Icon(Icons.add),
-          )
-        ],
-      ),
-      body: Column(
-      children: [
-        const Text('The chart'),
-        Expanded(
-          child: mainContent
+        appBar: AppBar(
+          title: const Text('Expense Tracker'),
+          actions: [
+            IconButton(
+              onPressed: _openAddExpenseOverlay,
+              icon: const Icon(Icons.add),
+            )
+          ],
         ),
-      ],
-    ));
+        body: width < 600 ? Column(
+          children: [
+            Chart(expenses: _registeredExpenses),
+            Expanded(child: mainContent),
+          ],
+        ) : Row(
+          children: [
+            Expanded(child: Chart(expenses: _registeredExpenses)),
+            Expanded(child: mainContent),
+          ],
+        )
+        );
   }
 }
